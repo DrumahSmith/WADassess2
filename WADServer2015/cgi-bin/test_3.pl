9b17852af010815666daafb6396a8d81b09dbeee
powellmark94 <powellmark94@gmail.com>
2015-04-07 23:46:34 +1200

    Deleted WAD
    Wouldn't upload all files

--- WADServer2015/cgi-bin/test_3.pl
+++
@@ -1,4 +0,0 @@
-#!perl
-use CGI qw(:standard);
-
-print header, start_html("Hello"), h1("Hello World"), p("Hello everybody."), end_html;
--- WADServer2015/core/palemoon/Bin/Palemoon/browser/defaults/preferences/firefox-l10n.js
+++
@@ -1,5 +0,0 @@
-//@line 4 "e:\mozdev\pm24ESR\browser\locales\en-US\firefox-l10n.js"
-
-//@line 6 "e:\mozdev\pm24ESR\browser\locales\en-US\firefox-l10n.js"
-
-pref("general.useragent.locale", "en-US");
