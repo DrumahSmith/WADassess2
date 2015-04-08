28dc27ae5547b1b11224ea9df18f8fa371b2562e
powellmark94 <powellmark94@gmail.com>
2015-04-08 13:19:41 +1200

    Create supporticket.php

---
+++ supporticket.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Assignment 2 Support Ticket
+Plugin URI: http://localhost/
+Description: Assignment 2, support ticket system for a help desk environment.
+Author: Chris Smith, Matt Ross, Mark Powell
+Version: 1.0
+Author URI: http://eitonline.eit.ac.nz/course/view.php?id=72/
+Last update: 
+*/
